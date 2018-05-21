0d4ca4ae40d8ce334ffd7d5d861c6209673ef7b2
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-05-22 00:53:21 +0300

    CLion project is added

---
+++ cmake-build-debug/CMakeFiles/Dictonary.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dictonary.dir/task2.c.obj"
+  "CMakeFiles/Dictonary.dir/Trie.c.obj"
+  "Dictonary.pdb"
+  "Dictonary.exe"
+  "Dictonary.exe.manifest"
+  "libDictonary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dictonary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
